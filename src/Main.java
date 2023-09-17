04323bd6c39ec7893e38687ec13eb137d00eb938
Lenusik8516 <helena19851606@gmail.com>
2023-09-17 11:56:43 +0300

    HW01

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Say your name: ");
+        String name = scanner.nextLine();
+
+
+        System.out.println("Welcome, " + name);
+        System.out.println("Hello World");
+
+    }
+}
